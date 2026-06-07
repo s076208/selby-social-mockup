24b5ae560982f29b4c809c00eeb05b7d30a623ed
s076208 <s076208@student.mnu.edu.mv>
2026-06-07 19:23:40 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 function toggleFB() {
-document.getElementById("fb-before").classList.toggle("hidden");
-document.getElementById("fb-after").classList.toggle("hidden");
+  document.getElementById("fb-before").classList.toggle("hidden");
+  document.getElementById("fb-after").classList.toggle("hidden");
 }
 
 function toggleIG() {
-document.getElementById("ig-before").classList.toggle("hidden");
-document.getElementById("ig-after").classList.toggle("hidden");
+  document.getElementById("ig-before").classList.toggle("hidden");
+  document.getElementById("ig-after").classList.toggle("hidden");
 }
