d42c2d3c62756ec7f9b874c1e3d546cd30728418
s076208 <s076208@student.mnu.edu.mv>
2026-06-07 17:31:20 +0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,19 @@
+function toggleFacebook() {
+
+let current = document.getElementById("fb-current");
+let improved = document.getElementById("fb-improved");
+
+current.classList.toggle("hidden");
+improved.classList.toggle("hidden");
+
+}
+
+function toggleInstagram() {
+
+let current = document.getElementById("ig-current");
+let improved = document.getElementById("ig-improved");
+
+current.classList.toggle("hidden");
+improved.classList.toggle("hidden");
+
+}
